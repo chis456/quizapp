6ccb1c4274afd5d9592bda78b8f12c7c2dd60736
Chris <55999720+chis456@users.noreply.github.com>
2024-02-02 15:57:41 -0800

    working nav

--- quizapp/target/classes/static/quizLogic.js
+++ quizapp/target/classes/static/quizLogic.js
@@ -1,9 +1,60 @@
 var questionnum = 1
+var navq2 = false
+var navq3 = false
 
 document.getElementById("Question2").style.display = "none"
 document.getElementById("Question3").style.display = "none"
 document.getElementById("Question4").style.display = "none"
 document.getElementById("navprev").disabled = true
+document.getElementById("navnext").disabled = true
+
+document.getElementById("1answer1").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+});
+document.getElementById("1answer2").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+});
+document.getElementById("1answer3").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+});
+document.getElementById("1answer4").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+});
+
+document.getElementById("2answer1").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq2 = true
+});
+document.getElementById("2answer2").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq2 = true
+});
+document.getElementById("2answer3").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq2 = true
+});
+document.getElementById("2answer4").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq2 = true
+});
+
+document.getElementById("3answer1").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq3 = true
+});
+document.getElementById("3answer2").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq3 = true
+});
+document.getElementById("3answer3").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq3 = true
+});
+document.getElementById("3answer4").addEventListener("click", function() {
+    document.getElementById("navnext").disabled = false
+    navq3 = true
+});
+
 
 document.getElementById("navnext").addEventListener("click", function() {
     if(questionnum == 1)
@@ -11,12 +62,22 @@ document.getElementById("navnext").addEventListener("click", function() {
         document.getElementById("navprev").disabled = false
         document.getElementById("Question1").style.display = "none"
         document.getElementById("Question2").style.display = "inline"
+
+        if(navq2 == false)
+        {
+            document.getElementById("navnext").disabled = true
+        }
     }
 
     else if(questionnum == 2)
     {
         document.getElementById("Question2").style.display = "none"
         document.getElementById("Question3").style.display = "inline"
+
+        if(navq3 == false)
+        {
+            document.getElementById("navnext").disabled = true
+        }
     }
 
     else if(questionnum == 3)
@@ -30,6 +91,8 @@ document.getElementById("navnext").addEventListener("click", function() {
     {
         questionnum += 1
     }
+
+    //document.getElementById("navnext").disabled = true
 });
 
 document.getElementById("navprev").addEventListener("click", function() {
@@ -58,5 +121,6 @@ document.getElementById("navprev").addEventListener("click", function() {
     {
         questionnum -= 1
     }
+    document.getElementById("navnext").disabled = false
     
 });
